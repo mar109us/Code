c1025458a21224a3a906fdd7f5225ce049da199c
Martinius Karlsen <mar109us@gmail.com>
2023-04-15 17:29:11 +0200

    commit test

--- fundamentals/age check/agecheck.js
+++ fundamentals/age check/agecheck.js
@@ -4,8 +4,6 @@ Write an if condition to check that age is between 14 and 90 inclusively.
 
 “Inclusively” means that age can reach the edges 14 or 90.
 
-test
-
 */
 
 let age = prompt("what is your age?");
