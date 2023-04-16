cde482634394ba5dbe5d1c5d8c544687e737b5b3
Martinius Karlsen <mar109us@gmail.com>
2023-04-16 17:11:14 +0200

    finished tasks

---
+++ Modern Javascript Tutorial/Fundamentals/While and for loops/whileFor.js
@@ -0,0 +1,70 @@
+//      while (condition) {
+//           code
+//           so-called "loop body"
+//      }
+
+// let a = 0;
+
+// while (a < 5){
+//     alert(a);
+//     a++;
+// }
+
+// if (a === 5){
+//     alert(a);
+// }
+
+// for (let i = 0; i < 3; i++){
+//     alert(i);
+// }
+
+// let i = 0;
+// let d = 3
+
+// for(; i < d; i++){
+// alert(i);
+// if (i === d){
+//     break;
+// }
+// }
+// alert(i);
+
+// for (let i = 0; i < 20; i++) {
+
+//     // if true, skip the remaining part of the body
+//     if (i % 2 == 0) continue;
+  
+//     alert(i); // 1, then 3, 5, 7, 9
+//   }
+
+// for (let i = 0; i < 3; i++) {
+
+//     for (let j = 0; j < 3; j++) {
+  
+//       let input = prompt(`Value at coords (${i},${j})`, '');
+  
+//       // what if we want to exit from here to Done (below)?
+//     }
+//   }
+  
+//   alert('Done!');
+
+
+// for (let i = 2; i <= 10; i++){
+//     if (i % 2 == 0){
+//         alert(i);
+//     }
+
+
+// }
+
+// for (let i = 0; i < 3; i++) {
+//     alert( `number ${i}!` );
+//   }
+
+let i = 0;
+
+while (i < 3){
+    alert( `number ${i++}!` );
+
+}
