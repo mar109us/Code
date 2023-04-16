8c38d16ba0496b145fa73fc1099e2390c6845fde
Martinius Karlsen <mar109us@gmail.com>
2023-04-16 11:33:19 +0200

    completed "defined" lesson

---
+++ Modern Javascript Tutorial/Fundamentals/FindFirstDefined/findDefined.js
@@ -0,0 +1,36 @@
+// let a;
+// let b = "hello";
+// let c;
+// let d;
+
+// // if none of the variables contain 
+// // data go to last and print "none defined"
+// alert(a ?? b ?? c ?? d ?? "none defined");
+
+// let firstName = null;
+// let lastName = null;
+// let nickName = "Supercoder";
+
+// // shows the first defined value:
+// alert(firstName ?? lastName ?? nickName ?? "Anonymous"); // Supercoder
+
+
+// let x = 1 && 2 ?? 3; //doesnt work
+
+// let x = (1 && 2) ?? 3;
+
+// let x = 1 && (2 ?? 3);
+
+// // set height=100, if height is null or undefined
+// let height;
+
+// height = height ?? 100;
+
+// alert(height);
+
+// height set to 50
+let height = 50;
+
+height = height ?? 100;
+
+alert(height);
