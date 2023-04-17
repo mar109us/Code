50a955ca2100551a8dcd9c3b4759962fa769ad7e
Martinius Karlsen <mar109us@gmail.com>
2023-04-17 19:16:33 +0200

    tried to program a calculator

---
+++ Modern Javascript Tutorial/Fundamentals/calculator test/calculator.js
@@ -0,0 +1,23 @@
+let firstInput = prompt("First number");
+let method = prompt("add or substract?");
+let secondInput = prompt("Second number");
+let result;
+let combinedInput = firstInput + secondInput;
+
+alert(firstInput + method + secondInput);
+
+alert(combinedInput);
+
+if (combinedInput === isNaN) {
+    alert("not numbers");
+} 
+else if (method.toLowerCase === "add" || "+") {
+    result = +firstInput + +secondInput;
+    alert(result);
+}
+else if (method.toLowerCase === "substract" || "-") {
+    result = +firstInput + +secondInput;
+    alert(result);
+}
+
+// works but some holes 
